4e0cc80bb804f2c181c9b81f0d245003da7a32b9
jinliang <jinliang@huami.com>
2020-11-11 21:08:44 +0800

    Initial commit

---
+++ src/day01/TestDemo01.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class TestDemo01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("huhu");
+    }
+}
---
+++ src/day01/TreeNode.java
@@ -0,0 +1,35 @@
+package day01;
+
+/**
+ * 构建treeNode 单次model
+ */
+public class TreeNode {
+
+    private int value ;
+    private TreeNode leftNode;
+    private TreeNode rightNode;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public TreeNode getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(TreeNode leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public TreeNode getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(TreeNode rightNode) {
+        this.rightNode = rightNode;
+    }
+}
